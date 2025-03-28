932774f42682dd43e3077bd7fdaf3d7e72009636
nmunshii <munshinadim55@gmail.com>
2025-03-27 22:39:47 -0400

    Added calendar page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,5 @@
 import Header from '@/components/ui/header';
-import { useAuth } from '../contexts/AuthContext';
-import { useNavigate } from 'react-router-dom';
-
 export default function Home() {
-
-
   return (
     <div className="min-h-svh" style={{ backgroundColor: '#BAA68E' }}>
       <Header />
